65397d07ca4ac23d17b9932807c87a6f5f63fa2a
Kamiethenerd <Kamiethenerd@gmail.com>
2016-05-11 16:54:53 -0500

    date stuff

--- web/app.php
+++ web/app.php
@@ -1,12 +1,5 @@
 <?php
 date_default_timezone_set('America/Chicago');
-$script_tz = date_default_timezone_get();
-
-if (strcmp($script_tz, ini_get('date.timezone'))){
-    echo 'Script timezone differs from ini-set timezone.';
-} else {
-    echo 'Script timezone and ini-set timezone match.';
-}
 
 use Symfony\Component\HttpFoundation\Request;
 
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,12 +1,6 @@
 <?php
-date_default_timezone_set('America/Chicago');
-$script_tz = date_default_timezone_get();
 
-if (strcmp($script_tz, ini_get('date.timezone'))){
-    echo 'Script timezone differs from ini-set timezone.';
-} else {
-    echo 'Script timezone and ini-set timezone match.';
-}
+date_default_timezone_set('America/Chicago');
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
